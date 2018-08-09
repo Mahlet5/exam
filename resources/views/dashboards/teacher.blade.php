c9eebc6d7d547fb36ca8764fab661f385c399c71
Eskindir <eskindir_ae@yahoo.com>
2018-08-09 15:53:02 +0300

    organized dashboards

--- resources/views/dashboards/teacher.blade.php
+++ resources/views/dashboards/teacher.blade.php
@@ -1,1 +1,32 @@
-dashboard elements appear here.
+<div class="content">
+  <div class="row">
+
+  @foreach($courses as $course)
+    <div class="col-md-3">
+      <div class="box box-primary">
+        <div class="box-header with-border" style="background-color:#697d9e;color:#fff;">
+          <h3 class="box-title">{{ $course->title }}</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+
+          <strong><i class="fa fa-book margin-r-5"></i>Manage</strong>
+
+          <p>
+            <span class="label label-warning">Students</span>
+            <span class="label label-info">Materials</span>
+            <span class="label label-success">Assignments</span>
+          </p>
+
+          <hr>
+
+          <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
+
+          <p>{{ $course->description }}</p>
+        </div>
+        <!-- /.box-body -->
+      </div>
+    </div>
+  @endforeach
+  </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,43 +6,14 @@ use App\Role;
 @endsection
 @section('content')
 {{-- select a dashboard to render --}}
-@if(Auth::user()->role_id==3)
-    <div class="content">
-      <div class="row">
-
-      @foreach($courses as $course)
-        <div class="col-md-3">
-        <div class="box box-primary">
-          <div class="box-header with-border" style="background-color:#697d9e;color:#fff;">
-            <h3 class="box-title">{{ $course->title }}</h3>
-          </div>
-          <!-- /.box-header -->
-          <div class="box-body">
-
-            <strong><i class="fa fa-book margin-r-5"></i>Manage</strong>
-
-            <p>
-              <span class="label label-warning">Students</span>
-              <span class="label label-info">Materials</span>
-              <span class="label label-success">Assignments</span>
-            </p>
-
-            <hr>
-
-            <strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
-
-            <p>{{ $course->description }}</p>
-          </div>
-          <!-- /.box-body -->
-        </div>
-          </div>
-      @endforeach
-      </div>
-    </div>
-
-    {{-- {{ $courses->links() }} --}}
-
+@if(Auth::user()->role_id==1)
+  @include('dashboards.admin');
+@elseif(Auth::user()->role_id==2)
+  @include('dashboards.student');
+@elseif(Auth::user()->role_id==3)
+  @include('dashboards.teacher',['courses'=>$courses]);
 @endif
+
 @endsection
 @section('postscripts')
 @endsection
